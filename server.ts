1629c884fd9acb2f36516b03b9ba0d5469dadc0e
Daniel Montet <dmontetproff@gmail.com>
2022-02-09 15:50:31 +0300

    first commit: react-express monolith setup

---
+++ server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/", function (_, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.get("/backend", (_, res) => {
+  res.send("Hello from server");
+});
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http:\\localhost${PORT}`);
+});
